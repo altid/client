911535aa2b3d706ca19e12d84e038b585c146276
Michael Misch <michaelmisch1985@gmail.com>
2016-07-26 23:41:47 -0600

    Not going to include these toolkits myself, PRs always accepted

---
+++ ui/nuklear.c
@@ -0,0 +1,23 @@
+// Update path to match your setup
+#include "../../nuklear/nuklear.h"
+#include "../ubiquitous.h"
+
+void
+initialize() {
+
+}
+
+void 
+navigate_back() {
+
+}
+
+void 
+navigate_forward() {
+
+}
+
+void
+destroy() {
+
+}

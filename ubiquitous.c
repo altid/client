afd6141b6089e6a77620dc3eac8f456fe5c7f9d4
Michael Misch <michaelmisch1985@gmail.com>
2016-07-30 10:35:53 -0600

    Kaiboshing YAML outright, starting work on json parsing

--- ubiquitous.c
+++ ubiquitous.c
@@ -2,19 +2,31 @@
 #include <jansson.h>
 #include "ubiquitous.h"
 
-
 // parse json from $prog.json
-// read in backend variable 
 // initialise the screen/terminal
 // connect filesystem in/out to UI elements
+//
 // run main loop, listening for key events or filesystem events
-//  - UI element interactions, etc
 
 int
 main(int argc, char *argv[]) {
-  (void) argc;
+  
+  if (argc != 2) {
+    fprintf(stderr, "usage: %s <%s.json>", argv[0], argv[0]);
+    return 2;
+  }
+
+  json_t *json;
+  json_error_t error;
+  //const char *path = argv[1];  
+  json = json_load_file(argv[1], 0, &error);
+  
+  if (!json)
+    fprintf(stderr, "Error loading %s: %s %d\n", argv[1], error.text, error.line);
+  
+
   initialize();
-  //set_title(argv[0]);
+//  set_title(argv[0]);
   destroy();
   return 0;
 }

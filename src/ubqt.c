9608845ceabdd57be7dd6e6420a32196bf93ad2e
Halfwit <michaelmisch1985@gmail.com>
2016-11-21 22:43:16 -0700

    Getting ready to completely restructure

--- src/ubqt.c
+++ src/ubqt.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
 
 #include "ubqt.h"
 
@@ -9,25 +7,21 @@ int
 main(int argc, char* argv[])
 {
 
-  if (argc != 2) {
-    fprintf(stderr, "usage: %s %s.toml\n", argv[0], argv[0]);
-    return 2;
-  }
+	if (argc != 2) {
+		fprintf(stderr, "usage: %s <path>\n", argv[0]);
+		return 2;
+	}
 
-  ubqt_win.menu_size = 0;
-  ubqt_win.buff_size = 0;
-  ubqt_win.navi_size = 0;
-  ubqt_win.current_out = "/home/halfwit/irc/irc.freenode.net/#teamcool/out";
-  ubqt_win.current_in = "/home/halfwit/irc/irc.freenode.net/#teamcool/in";
-  ubqt_win.path = "/home/halfwit/irc/irc.freenode.net";
+	/* If plan9, query for CPU + auth + password
+	* If on regular filesystem we don't need this
+	*/
+	ubqt_connection_init(argv[1]);
 
-  /* Set up windows and handle mounting if necessary */
-  ubqt_initialize(argv[0]);
-  
-  /* Interactive loop */
-  ubqt_run_loop();
+	ubqt_window_init();
 
-  ubqt_destroy();
+	ubqt_run_loop();
 
-  return 0;
+	ubqt_destroy();
+
+	return 0;
 }

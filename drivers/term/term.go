5f8695634fd6605403b52c572da9b0e8461450c2
Michael Misch <michealmisch1985@gmail.com>
2021-05-28 13:41:32 -0700

    Add in some notes about how it should work

--- drivers/term/term.go
+++ drivers/term/term.go
@@ -1,1 +1,6 @@
 package term
+
+// We are passed back blocks with the content we are to draw for each area
+// Blocks contain a description of an area, and are ordered
+// Blocks can be nested inside other blocks, to allow sub-structure.
+// A block can have a set width, set height, both, or grow to fill the remaining parent space
--- internal/draw/draw.go
+++ internal/draw/draw.go
@@ -1,2 +1,4 @@
 package draw
 
+// Marshall json into structure
+// Create blocks, stack them according to the json description, then send the schema off to the driver to render
